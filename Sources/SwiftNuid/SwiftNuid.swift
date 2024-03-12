7c2050351e370b3ba33a51a3545c201e824966be
Jarema <melgaer@gmail.com>
2024-03-12 09:45:11 +0100

    Add NUID functionality
    Signed-off-by: Jarema <melgaer@gmail.com>

---
+++ Sources/SwiftNuid/SwiftNuid.swift
@@ -0,0 +1,95 @@
+import Foundation
+
+// Version of the library
+let Version = "1.0.1"
+
+let DIGITS = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".utf8)
+let BASE = 62
+let PRE_LEN = 12
+let SEQ_LEN = 10
+let MAX_SEQ = Int64(839299365868340224) // base^seqLen == 62^10
+let MIN_INC = Int64(33)
+let MAX_INC = Int64(333)
+let TOTAL_LEN = PRE_LEN + SEQ_LEN
+
+class NUID {
+    var pre: [UInt8]
+    var seq: Int64
+    var inc: Int64
+
+    init() {
+        self.pre = [UInt8](repeating: 0, count: PRE_LEN)
+        self.seq = Int64.random(in: 0..<MAX_SEQ)
+        self.inc = MIN_INC + Int64.random(in: 0..<(MAX_INC - MIN_INC))
+        self.randomizePrefix()
+    }
+
+    func next() -> String {
+        self.seq += self.inc
+        if self.seq >= MAX_SEQ {
+            self.randomizePrefix()
+            self.resetSequential()
+        }
+        let seq = self.seq
+
+        var b = [UInt8](repeating: 0, count: TOTAL_LEN)
+        var bs = b[0..<PRE_LEN]
+        bs.replaceSubrange(bs.startIndex..<bs.endIndex, with: self.pre)
+
+        var i = b.count
+        var l = seq
+        while i > PRE_LEN {
+            i -= 1
+            b[i] = DIGITS[Int(l % Int64(BASE))]
+            l /= Int64(BASE)
+        }
+
+        return String(decoding: b, as: UTF8.self)
+    }
+
+    func resetSequential() {
+        self.seq = Int64.random(in: 0..<MAX_SEQ)
+        self.inc = MIN_INC + Int64.random(in: 0..<(MAX_INC - MIN_INC))
+    }
+
+    func randomizePrefix() {
+        var cb = [UInt8](repeating: 0, count: PRE_LEN)
+        let result = SecRandomCopyBytes(kSecRandomDefault, PRE_LEN, &cb)
+
+        if result != errSecSuccess {
+            fatalError("nuid: failed generating crypto random number")
+        }
+
+        for i in 0..<PRE_LEN {
+            self.pre[i] = DIGITS[Int(cb[i]) % BASE]
+        }
+    }
+}
+
+class LockedNUID {
+    private var lock = NSLock()
+    var nuid: NUID
+
+    init() {
+        self.nuid = NUID()
+    }
+
+    func next() -> String {
+        lock.lock()
+        defer { lock.unlock() }
+        return nuid.next()
+    }
+}
+
+// Global NUID
+var globalNUID = LockedNUID()
+
+// Generate the next NUID string from the global locked NUID instance.
+func Next() -> String {
+    return globalNUID.next()
+}
+
+// Generate a new NUID instance.
+func New() -> NUID {
+    return NUID()
+}
---
+++ Tests/SwiftNuidTests/SwiftNuidTests.swift
@@ -0,0 +1,54 @@
+import XCTest
+@testable import SwiftNuid
+
+
+final class swiftNuidTests: XCTestCase {
+    func testDigits() {
+        XCTAssertEqual(DIGITS.count, BASE, "Digits length does not match base modulo")
+    }
+
+    func testGlobalNUIDInit() {
+        XCTAssertNotNil(globalNUID, "Expected globalNUID to be non-nil")
+        XCTAssertEqual(globalNUID.nuid.pre.count, PRE_LEN, "Expected prefix to be initialized")
+        XCTAssertNotEqual(globalNUID.nuid.seq, 0, "Expected seq to be non-zero")
+    }
+
+    func testNUIDRollover() {
+        globalNUID.nuid.seq = MAX_SEQ
+        let oldPre = globalNUID.nuid.pre
+        _ = Next()
+        XCTAssertNotEqual(globalNUID.nuid.pre, oldPre, "Expected new pre, got the old one")
+    }
+
+    func testGUIDLen() {
+        let nuid = Next()
+        XCTAssertEqual(nuid.count, TOTAL_LEN, "Expected len of \(TOTAL_LEN), got \(nuid.count)")
+    }
+
+    func testProperPrefix() {
+        var min: UInt8 = 255
+        var max: UInt8 = 0
+
+        for digit in DIGITS {
+            if digit < min {
+                min = digit
+            }
+            if digit > max {
+                max = digit
+            }
+        }
+
+        let total = 100000
+
+        for _ in 0..<total {
+            let n = New()
+            for j in 0..<PRE_LEN {
+                XCTAssertGreaterThanOrEqual(n.pre[j], min, "Valid range for bytes prefix: [\(min)..\(max)]")
+                XCTAssertLessThanOrEqual(n.pre[j], max, "Valid range for bytes prefix: [\(min)..\(max)]")
+            }
+        }
+    }
+}
+
+
+
