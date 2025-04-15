8fe9cd10470fc53069738e17db6b4f62c8f44aa7
shadmazumder <shadmazumder@gmail.com>
2025-04-15 15:29:34 +0600

    Add support for linux os

--- Sources/Nuid/Nuid.swift
+++ Sources/Nuid/Nuid.swift
@@ -1,5 +1,11 @@
 import Foundation
 
+#if os(Linux)
+import Glibc
+#else
+import Security
+#endif
+
 let DIGITS = Array("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".utf8)
 let BASE = 62
 let PRE_LEN = 12
@@ -45,22 +51,41 @@ public class Nuid {
 
         return String(decoding: b, as: UTF8.self)
     }
+
     func resetSequential() {
         self.seq = Int64.random(in: 0..<MAX_SEQ)
         self.inc = MIN_INC + Int64.random(in: 0..<(MAX_INC - MIN_INC))
     }
 
-     func randomizePrefix() {
+    func randomizePrefix() {
         var cb = [UInt8](repeating: 0, count: PRE_LEN)
-        let result = SecRandomCopyBytes(kSecRandomDefault, PRE_LEN, &cb)
-
-        if result != errSecSuccess {
-            fatalError("nuid: failed generating crypto random number")
-        }
+        generateSecureRandomBytes(&cb, count: PRE_LEN)
 
         for i in 0..<PRE_LEN {
             self.pre[i] = DIGITS[Int(cb[i]) % BASE]
         }
+    }
+
+    private func generateSecureRandomBytes(_ buffer: inout [UInt8], count: Int) {
+        #if os(Linux)
+        // Use /dev/urandom for Linux
+        let file = fopen("/dev/urandom", "rb")
+        guard file != nil else {
+            fatalError("nuid: failed to open /dev/urandom")
+        }
+        defer { fclose(file) }
+
+        let bytesRead = fread(&buffer, 1, count, file)
+        guard bytesRead == count else {
+            fatalError("nuid: failed to read random bytes from /dev/urandom")
+        }
+        #else
+        // Use SecRandomCopyBytes for macOS
+        let result = SecRandomCopyBytes(kSecRandomDefault, count, &buffer)
+        guard result == errSecSuccess else {
+            fatalError("nuid: failed generating crypto random number")
+        }
+        #endif
     }
 }
 
